df3a08172e36674d82db17349b51dbb2ea28c0f4
ayaBahgt <Aya_Bahjat_1219@fci.helwan.edu.eg>
2021-08-13 00:55:18 +0200

    create task3

---
+++ task3.py
@@ -0,0 +1,17 @@
+# This is a sample Python script
+def divisible(N,number):
+ for i in range(num):
+  if number[i]%N==0:
+   yes.append(number[i])
+ return (yes)
+n=int(input("please enter number you want divisible by"))
+number=[]
+yes=[]
+num=int(input("enter the number of element"))
+print("enter the list",end=" ")
+for el in range(0,num,1):
+ ele=int(input())
+ number.append(ele)
+divisible(n,number)
+print("the number in ",number,"that are divisible by",n,"are",yes)
+
